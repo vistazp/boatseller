18ac90854edabb926b5b10c3228f510bd6b432be
Vildtop <Vildtop@192.168.1.4>
2015-11-17 11:56:00 +0200

    post2

--- views/details/js/pit.js
+++ views/details/js/pit.js
@@ -21,31 +21,41 @@ $(function () {
 			return false;
 		});
             });  
-    });
+         });
 
 $(document).ready(function(){
-           
-    $('.fileinput').change(function () {
+       $('.fileinput').change(function () {
 
            // return false;
-   var send_url = $(this).attr('data-url');
-   var fd = new FormData();
+           var send_url = $(this).attr('data-url');
+           var fd = new FormData();
  
- for (var i = 0; i <= this.files.length; i++)
- {
-      fd.append("userfile[]", this.files[i]);
-  }      
+           for (var i = 0; i <= this.files.length; i++)
+            {
+             fd.append("userfile[]", this.files[i]);
+            }      
   
-    $.ajax({
-        url: send_url,
-        type: 'POST',
-        enctype: 'multipart/form-data',
-        data: fd,
-        processData: false,
-        contentType: false,
-     
-           });
-     
-    });
+            $.ajax({
+            url: send_url,
+            type: 'POST',
+            enctype: 'multipart/form-data',
+            data: fd,
+            processData: false,
+            contentType: false,
+            success: function(){
+                $.get('/boatseller/details/getPicList', function(data) {
+                v = JSON.parse(data);
+		
+		for (var i = 0; i < v.length; i++)
+		{
+                  // <img src="pic_mountain.jpg" alt="Mountain View" style="width:304px;height:228px;">
+		$('#picinsert').append('<div><img src=' + v[i].path + ' style="width:250px;"> <a class="del" rel="'+v[i].id+'" href="#">X</a></div>');
+              //   alert(v[i].path);
+		}
+                  return false;
+		});
+              }
+             });
+           });         
        
         });
